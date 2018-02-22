4570e224b1dd66fcdcf8e11e6a592f7b249a861a
denzil davan <31404103@mylife.unisa.ac.za>
2018-02-22 16:05:00 +0200

    SUM

--- stock.py
+++ stock.py
@@ -1,5 +1,5 @@
 import fdb
-
+import collections
 class StockItem:
     def __init__(self):
         self.MN=""
@@ -9,35 +9,46 @@ class StockItem:
         self.DISP=0
         self.UP=0
         self.NC=""
+        self.SOH=0
 
 def getDisp(item):
-    vdate=str(item.STDATE)[0:10]
-   
+    vdate=str(item.STDATE)[0:10]   
     cur.execute("select count(QUANTITY) from medicineregister where NAPPICODE="+item.NC+" and vdate>=CAST('"+vdate+"' AS TIMESTAMP)")
     arr=cur.fetchone()   
     item.DISP=arr[0]
+
+def getName(item):
+    cur.execute("select MN from MD2 where NC="+item.NC)
+    arr=cur.fetchone()   
+    item.MN=arr[0]
+
+def printSameStock(SSList):
+    A=3
+
+
  
-con = fdb.connect(dsn='C:/Users/dtdav/Desktop/NONGOMA.GDB', user='SYSDBA', password='masterkey')
+#con = fdb.connect(dsn='C:/Users/dtdav/Desktop/NONGOMA.GDB', user='SYSDBA', password='masterkey')
+con = fdb.connect(dsn='C:/Users/Dan/Desktop/20180219/NONGOMA.GDB', user='SYSDBA', password='masterkey')
 
 # Create a Cursor object that operates in the context of Connection con:
 cur = con.cursor()
 
 # Execute the SELECT statement:
-cur.execute("select NC,SAME,STDATE,STOCKQ from STOCK")
+cur.execute("select NC,SAME,STDATE,STOCKQ from STOCK ORDER BY SAME")
 
 # Retrieve all rows as a sequence and print that sequence:
 arr=cur.fetchall()
 i=-1
 siList=[]
-for l in arr:    
-    i=StockItem()
-    i.NC=l[0]
-    i.SAME=l[1]
-    i.STDATE=l[2]
-    i.STOCKQ=l[3]
-    getDisp(i)
-    print("nc= "+ i.NC+" disp="+str(i.DISP))
-    siList.append(i)
+samesiList=[]
+zeroList=[]
+num=len(arr)
+m = sum(1 for i in arr if i[1]==0)
+print(m)
+
+
+
+
 
 
 
