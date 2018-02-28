03839bf224a0ebc8cd6260d3c8033cadad4b2001
denzil davan <31404103@mylife.unisa.ac.za>
2018-02-28 14:58:59 +0200

    SAME

--- stock.py
+++ stock.py
@@ -12,7 +12,7 @@ class StockItem:
         self.SOH=0
 
     def __str__(self):
-        return '__str__ for Car'
+        return self.MN+" "+str(self.SAME)
 
 def getDisp(item):
     vdate=str(item.STDATE)[0:10]   
@@ -20,39 +20,46 @@ def getDisp(item):
     arr=cur.fetchone()   
     item.DISP=arr[0]
 
-def getName(item):
-    cur.execute("select MN from MD2 where NC="+item.NC)
+def getName(si):
+    cur.execute("select MN from MD2 where NC="+si.NC)
     arr=cur.fetchone()   
-    item.MN=arr[0]
+    try:
+       si.MN=arr[0]
+    except:
+       print("getName Error for : "+si.NC)
+       
 
 def getSame(siL,s):
     ls=(i for i in siL if i.SAME==s)
+    for x in ls:
+        print("getSame"+ x.MN+" "+x.NC+" "+str(x.SAME)+str(x.UP))
     soh=sum(i.SOH for i in ls) 
-    up=max(i.UP for i in ls)  
-    print("TOTAL QUANTITY = "+str(soh)+ " TOTAL PRICE = " +str(soh*up))
+    up=max(for i in ls)  
+    print("getSame TOTAL QUANTITY = "+str(soh)+ " TOTAL PRICE = " +str(soh*up))
     for i in ls:
         print(i.MN+"\t"+i.SOH)    
     print("_____________________")
 
     
 def getUP(si):
+    print("class "+str(si))
     cur.execute("select UNITPRICE from PURCHASES where NC="+ si.NC +" ORDER BY PDATE DESC")
     arr=cur.fetchone()   
     try:
 
        si.UP=arr[0]
     except:
-       print("Error for : "+si.NC)
+       print("getUP Error for : "+si.NC)
        si.UP=0
  
-con = fdb.connect(dsn='C:/Users/dtdav/Desktop/NONGOMA.GDB', user='SYSDBA', password='masterkey')
-#con = fdb.connect(dsn='C:/Users/Dan/Desktop/20180219/NONGOMA.GDB', user='SYSDBA', password='masterkey')
+#con = fdb.connect(dsn='C:/Users/dtdav/Desktop/NONGOMA.GDB', user='SYSDBA', password='masterkey')
+con = fdb.connect(dsn='C:/Users/Dan/Desktop/20180219/NONGOMA.GDB', user='SYSDBA', password='masterkey')
 
 # Create a Cursor object that operates in the context of Connection con:
 cur = con.cursor()
 
 # Execute the SELECT statement:
-cur.execute("select first 10 NC,SAME,STDATE,STOCKQ from STOCK ORDER BY SAME ")
+cur.execute("select first 10 NC,SAME,STDATE,STOCKQ from STOCK WHERE SAME!=0 ORDER BY SAME ")
 
 # Retrieve all rows as a sequence and print that sequence:
 arr=cur.fetchall()
@@ -68,14 +75,16 @@ for i in arr:
     si.SAME=i[1]
     si.STDATE=i[2]
     si.STOCKQ=i[3]
+    getName(si)
     getDisp(si)
     si.SOH=si.STOCKQ-si.DISP
     getUP(si)
     siList.append(si)
 
 s=set(samesiList)
-s.remove(0)
+#s.remove(0)
 for x in s:
+    print("x in s -- "+str(x))
     getSame(siList,x)
 
 
